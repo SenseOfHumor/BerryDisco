0f27cf048646777eb98bb9dc058d250c3d9d144a
Marvinda0 <jesusm.garcialonso@gmail.com>
2024-09-29 09:50:16 -0400

    Fix

---
+++ config.py
@@ -0,0 +1,6 @@
+settings = {
+    'host': 'https://berry-disco.documents.azure.com:443/',  # Corrected Cosmos DB URI
+    'master_key': 'J7KZEROOI8TNld12WkNnGc50PAFksFapXQAQ9ExCYOrdRVyjYBzdcZuWpX4Ghrxrp9cz8KrLT0w6ACDbPTYyKA==',  # Your Cosmos DB Master Key
+    'database_id': 'BerryDiscoDB',       # Your Cosmos DB Database ID
+    'container_id': 'BerryList'    # Your Cosmos DB Container ID
+}
